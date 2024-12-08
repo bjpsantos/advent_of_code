0ee23af4607c981c20aadd4dcd6c2d0548cec6f0
bjpsantos <bjp.santos@hotmail.com>
2024-12-08 21:59:37 +0000

    day3 part2 done

--- advent-of-code/src/2024/Day3/main.py
+++ advent-of-code/src/2024/Day3/main.py
@@ -16,10 +16,11 @@ class Puzzle:
         self.input = "".join(text_input)
 
         self.regex_pattern = r"mul\((\d{1,3}),(\d{1,3})\)"
+        self.regex_pattern_cond = r"mul\((\d{1,3}),(\d{1,3})\)|(do|don\'t)\(\)"
 
-    def get_mul_sections(self):
-        mul_sections = re.findall(self.regex_pattern, self.input)
-        return mul_sections
+    def get_sections(self, pattern):
+        sections = re.findall(pattern, self.input)
+        return sections
 
     def sum_mul_sections(self, mul_sections):
         result = 0
@@ -28,15 +29,29 @@ class Puzzle:
 
         return result
 
+    def conditional_sum_sections(self, sections):
+        enable = True
+
+        result = 0
+        for section in sections:
+            if section[2] == "don't":
+                enable = False
+            elif section[2] == "do":
+                enable = True
+            else:
+                if enable:
+                    result = result + int(section[0]) * int(section[1])
+
+        return result
+
     def solve(self, part):
         if part == 1:
-            mul_sections = self.get_mul_sections()
+            mul_sections = self.get_sections(pattern=self.regex_pattern)
             return self.sum_mul_sections(mul_sections)
 
-        # if part == 2:
-        #     list_A, list_B = self.create_lists()
-        #     similarity_score = self.get_similarity_score(list_A, list_B)
-        #     return sum(similarity_score)
+        if part == 2:
+            sections = self.get_sections(pattern=self.regex_pattern_cond)
+            return self.conditional_sum_sections(sections)
 
 
 @timing_decorator
@@ -50,20 +65,20 @@ def main(raw, part):
 
 def run_tests():
     print(f"\nRunning Tests:")
-    # assert main(raw=files["test"], part=1) == 161
-    # assert main(raw=files["test"], part=2) == 31
+    assert main(raw=files["test"], part=1) == 161
+    assert main(raw=files["test2"], part=2) == 48
 
     # solutions
-    # print(f"\nRunning Solutions:")
-    # assert main(raw=files["input"], part=1) == 55971
+    print(f"\nRunning Solutions:")
+    assert main(raw=files["input"], part=1) == 179571322
 
 
 def solve():
     print(f"\nSolving:")
     answer1 = main(raw=files["input"], part=1)
     print(f"Answer part1: {magenta_color}{answer1}{reset_color}")
-    # answer2 = main(raw=files["input"], part=2)
-    # print(f"Answer part2: {magenta_color}{answer2}{reset_color}")
+    answer2 = main(raw=files["input"], part=2)
+    print(f"Answer part2: {magenta_color}{answer2}{reset_color}")
 
 
 if __name__ == "__main__":
